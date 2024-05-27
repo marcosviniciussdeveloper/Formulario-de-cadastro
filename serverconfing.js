4281c827e36520d846e7ca20d4a2b3d6885e4b17
Marcos vinicius <110131336+marcosviniciussdeveloper@users.noreply.github.com>
2024-05-27 13:15:16 -0300

    server_config

---
+++ serverconfing.js
@@ -0,0 +1,26 @@
+const express = require ("express");
+const app = express(); 
+
+
+app.get("/cadastro" , function(req , res){
+    res.send("Bem vindo!")
+
+
+
+    function escrever() {
+
+        console.log ("escreva de 1 até 2 ")
+
+
+    
+    }
+
+
+});
+
+//localhost:(porta)
+app.listen(8586 , function(){
+
+console.log("servidor rodando na url http://localhost:8586")
+
+});
